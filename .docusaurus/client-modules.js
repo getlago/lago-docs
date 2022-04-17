da3948704a7b1f53fded4347f617936d16e58428
Jeremy Denquin <jdenquin@gmail.com>
2022-04-17 17:56:10 +0200

    remove docusaurus folder

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/michaelponrajah/Documents/lago-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/michaelponrajah/Documents/lago-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/michaelponrajah/Documents/lago-docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/michaelponrajah/Documents/lago-docs/src/css/custom.css'),
-];
