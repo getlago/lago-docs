83f03d04784192adb39c0f8bae2e8dc1566d5414
Jeremy Denquin <jdenquin@gmail.com>
2022-04-17 17:51:51 +0200

    remove useless folders

--- build/assets/js/041a96f5.71be1043.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[66],{3769:function(e){e.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-docs","id":"default"}}')}}]);
