e2feed2a43e589a7923654e5f087c941c8df8828
Raffi Sarkissian <raffi@MacBook-Pro-de-Raffi.local>
2022-05-06 20:17:22 +0200

    improve customers api reference

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,6 +58,11 @@ const config = {
             position: 'left',
             label: 'Guide',
           },
+          {
+            to: '/blog',
+            label: 'API Reference',
+            position: 'left'
+          },
           {to: 'https://getlago.com/', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
